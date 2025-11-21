a3a4f1b004c03fb8d914a830637ebda9f7ba21a2
cyruscvc <cyruscvc@gmail.com>
2025-11-21 11:09:26 +0400

    Update screenguard.js

--- www/screenguard.js
+++ www/screenguard.js
@@ -1,11 +1,19 @@
-var exec = require("cordova/exec");
+var exec = require('cordova/exec');
 
 exports.start = function(callback) {
     window.SCREEN_GUARD_CB = callback;
 
-    // iOS: start observers
-    exec(null, null, "ScreenGuard", "startObservers", []);
+    // iOS: start observers (if plugin class exists)
+    try {
+        exec(null, null, "ScreenGuard", "startObservers", []);
+    } catch (e) {
+        // ignore if not available
+    }
 
     // Android: enable FLAG_SECURE
-    exec(null, null, "ScreenGuard", "enableSecure", []);
+    try {
+        exec(null, null, "ScreenGuard", "enableSecure", []);
+    } catch (e) {
+        // ignore if not available
+    }
 };
