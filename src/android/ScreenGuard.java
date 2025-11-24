03f8050945a4fd616c9d452b4fefcb8ce717cc0e
cyruscvc <cyruscvc@gmail.com>
2025-11-24 16:32:58 +0400

    Update ScreenGuard.java

--- src/android/ScreenGuard.java
+++ src/android/ScreenGuard.java
@@ -2,53 +2,45 @@ package cordova.plugin.screenguard;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.PluginResult;
-
-import android.view.WindowManager;
-import android.util.Log;
-
 import org.json.JSONArray;
+import android.view.WindowManager;
 
 public class ScreenGuard extends CordovaPlugin {
 
-    private static final String TAG = "ScreenGuard";
+    @Override
+    public void pluginInitialize() {
+        applySecureFlag();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-        if ("start".equals(action) || "enable".equals(action)) {
-            this.cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    cordova.getActivity().getWindow().setFlags(
-                        WindowManager.LayoutParams.FLAG_SECURE,
-                        WindowManager.LayoutParams.FLAG_SECURE
-                    );
-                    Log.d(TAG, "FLAG_SECURE enabled – screenshots & recordings blocked");
-                    callbackContext.success("ScreenGuard activated");
-                }
-            });
+        if ("start".equals(action)) {
+            applySecureFlag();
+            callbackContext.success("started");
             return true;
         }
-
-        if ("stop".equals(action) || "disable".equals(action)) {
-            this.cordova.getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
-                    Log.d(TAG, "FLAG_SECURE disabled");
-                    callbackContext.success("ScreenGuard deactivated");
-                }
-            });
+        if ("stop".equals(action)) {
+            cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+            callbackContext.success("stopped");
             return true;
         }
-
         return false;
     }
+
     @Override
     public void onResume(boolean multitasking) {
-        cordova.getActivity().getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE
-        );
+        applySecureFlag();
+    }
+
+    private void applySecureFlag() {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                cordova.getActivity().getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_SECURE,
+                    WindowManager.LayoutParams.FLAG_SECURE
+                );
+            }
+        });
     }
 }
